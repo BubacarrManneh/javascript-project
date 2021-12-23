dabf638da134c56a4eeb9e982dd00e14a044a5fd
BubacarrManneh <bubacarr.manneh@integrify.io>
2021-12-23 20:20:11 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Classes from './Style.module.css'
+
+function App() {
+  return (
+    <div className={Classes.App}>
+     <header>Buba's Todo-App</header>
+    </div>
+  );
+}
+
+export default App;
