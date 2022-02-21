f6bb88b77c8250dc7d7376e0d835c02f38aa710b
BubacarrManneh <bubacarr.manneh@integrify.io>
2022-02-21 21:58:23 +0100

    Fetching Weather API and DOM manupulation

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,1 +1,23 @@
+const key = "mFZqgyB9yShFdIxMwWUlB8AAu1FTRZHD";
+
+// Current weather of the city
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const req = await fetch(base + query);
+  const res = await req.json();
+  return res[0];
+};
+
+// City weather
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const request = await fetch(base + query);
+  const data = await request.json();
+    return data[0];
+  //   console.log(data[0]);
+};
 
