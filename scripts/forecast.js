0ce30b9f868b859f0007297baf7496f14835cce9
BubacarrManneh <bubacarr.manneh@integrify.io>
2022-02-25 16:53:47 +0100

    Secure link

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -2,7 +2,7 @@ const key = "mFZqgyB9yShFdIxMwWUlB8AAu1FTRZHD";
 
 // Current weather of the city
 const getWeather = async (id) => {
-  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const base = "https://dataservice.accuweather.com/currentconditions/v1/";
   const query = `${id}?apikey=${key}`;
 
   const req = await fetch(base + query);
@@ -12,7 +12,7 @@ const getWeather = async (id) => {
 
 // City weather
 const getCity = async (city) => {
-  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const base = "https://dataservice.accuweather.com/locations/v1/cities/search";
   const query = `?apikey=${key}&q=${city}`;
 
   const request = await fetch(base + query);
